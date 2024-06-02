443c65d1663e2136167b7a75f36addad5d8e5d9c
SushilkumarDev <sushilkumardeveloper.com>
2024-06-02 23:29:26 +0530

    arpSpoof file updated

--- ARP Spoof/arpSpoof.py
+++ ARP Spoof/arpSpoof.py
@@ -29,6 +29,7 @@ def get_arguments() -> Tuple[str, str]:
     args = parser.parse_args()
     args = parser.parse_args()
     args = parser.parse_args()
+    
     args = parser.parse_args()
     args = parser.parse_args()
     args = parser.parse_args()

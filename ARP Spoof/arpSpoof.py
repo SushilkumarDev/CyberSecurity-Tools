b11cad555e94dd9b13388f183e543972c36d3a2b
SushilkumarDev <sushilkumardeveloper.com>
2024-06-02 23:17:28 +0530

    for target ip address

--- ARP Spoof/arpSpoof.py
+++ ARP Spoof/arpSpoof.py
@@ -3,3 +3,8 @@ import time
 import sys
 from typing import Tuple
 from scapy.all import ARP, Ether, srp, sendp
+
+
+def get_arguments() -> Tuple[str, str]:
+    """Parses command-line arguments and returns target and gateway IP addresses."""
+    parser = argparse.ArgumentParser()
